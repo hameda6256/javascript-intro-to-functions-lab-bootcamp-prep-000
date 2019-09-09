ea80fae863d8e94324c5d8f22c3f3b379cdaf949
Adham  Hamed <hameda6256@gmail.com>
2019-09-09 14:35:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ function sayHiToGrandma (string){
   var cantAnswer = "I cant hear you!";
   var yesAnswer = "YES INDEED!";
   var loveAnswer = "I love you, too.";
-    if (string
+    if (string.toLowerCase
 }
