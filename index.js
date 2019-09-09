80ca077b01386250c1c53e4a17bc2335e8ed4cee
Adham  Hamed <hameda6256@gmail.com>
2019-09-09 14:42:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string){
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma (string){
-  var cantAnswer = "I cant hear you!";
+  var cantAnswer = "I can't hear you!";
   var yesAnswer = "YES INDEED!";
   var loveAnswer = "I love you, too.";
     if (string.toLowerCase(string) === string){
