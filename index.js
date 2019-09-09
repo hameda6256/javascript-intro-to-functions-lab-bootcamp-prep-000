8812948e96506e17cdd5346775a894dd28610233
Adham  Hamed <hameda6256@gmail.com>
2019-09-09 14:19:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ function shout(string) {
   return string.toUpperCase();
 }
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
 function logShout(string) {
-  console.log(string.toUpperCase)())
+  console.log(string.toUpperCase());
 }
 logShout()
